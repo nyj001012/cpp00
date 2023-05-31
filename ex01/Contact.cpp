564822570d5a1cb350109afc139777f23876c211
yena <50291995+nyj001012@users.noreply.github.com>
2023-05-31 14:50:31 +0900

    feat: implement Contact class

---
+++ ex01/Contact.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 14:06:56 by yena              #+#    #+#             */
+/*   Updated: 2023/05/31 14:50:08 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.h"
+
+Contact::Contact(std::string first_name, std::string last_name,
+				 std::string nickname, std::string phone_number,
+				 std::string darkest_secret)
+{
+	this->first_name = first_name;
+	this->last_name = last_name;
+	this->nickname = nickname;
+	this->phone_number = phone_number;
+	this->darkest_secret = darkest_secret;
+}
---
+++ ex01/Contact.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 14:06:56 by yena              #+#    #+#             */
+/*   Updated: 2023/05/31 14:50:16 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CPP00_CONTACT_H
+#define CPP00_CONTACT_H
+
+class Contact
+{
+private:
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string darkest_secret;
+};
+
+#endif

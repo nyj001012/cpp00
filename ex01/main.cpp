ef24f0b6afac44f7590575d7ec0376b39ebedfbe
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 08:41:26 +0900

    feat: add main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/31 14:06:45 by yena              #+#    #+#             */
-/*   Updated: 2023/05/31 14:55:38 by yena             ###   ########.fr       */
+/*   Created: 2023/05/31 14:06:17 by yena              #+#    #+#             */
+/*   Updated: 2023/06/05 08:40:38 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Contact.hpp"
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(Contact contact, int index)
+int	main(void)
 {
-	this->contacts[index] = contact;
+	std::string command;
+
+	while (true)
+	{
+		command = std::cin.get();
+		std::cout << "command: " << command << std::endl;
+		if (command == "EXIT")
+			break ;
+		else if (command == "ADD")
+			std::cout << "ADD" << std::endl;
+		else if (command == "SEARCH")
+			std::cout << "SEARCH" << std::endl;
+		else
+			std::cout << "Invalid command" << std::endl;
+	}
+	return (0);
 }

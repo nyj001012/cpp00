7bbdd18ba04ccc2c59f0ddec1c65eee7c76583dc
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 14:59:47 +0900

    chore: apply google convention

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,24 +6,20 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:49:56 by yena              #+#    #+#             */
-/*   Updated: 2023/05/31 14:03:19 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 14:59:42 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	if (argc > 1)
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)std::toupper(argv[i][j]);
-		}
-		std::cout << std::endl;
-	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	return (0);
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      for (int j = 0; argv[i][j]; j++)
+        std::cout << (char) std::toupper(argv[i][j]);
+    }
+    std::cout << std::endl;
+  } else
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  return (0);
 }

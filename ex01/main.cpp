e8e22db969f9713f3d41dac4de623713e2381fc9
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 11:38:51 +0900

    feat: add object of PhoneBook

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,33 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/05 09:03:29 by yena              #+#    #+#             */
-/*   Updated: 2023/06/05 09:03:52 by yena             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:06:17 by yena              #+#    #+#             */
-/*   Updated: 2023/06/05 09:03:29 by yena             ###   ########.fr       */#.fr       */
-=======
-/*   Updated: 2023/06/05 08:55:33 by yena             ###   ########.fr       */
->>>>>>> parent of 184bbc3 (chore: change if-else to switch-case)
+/*   Updated: 2023/06/05 11:32:43 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include "Contact.hpp"
-#include "PhoneBook.hpp"
+#include "main.hpp"
 
 int	main(void)
 {
-	std::string command;
+	std::string	command;
+	PhoneBook	phonebook;
 
 	while (true)
 	{
@@ -40,7 +24,7 @@ int	main(void)
 		if (command == "EXIT")
 			break ;
 		else if (command == "ADD")
-			std::cout << "ADD" << std::endl;
+			AddContact(&phonebook);
 		else if (command == "SEARCH")
 			std::cout << "SEARCH" << std::endl;
 		else

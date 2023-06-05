08664c83a940184f38b4c4a8a15af788efd54676
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 14:36:10 +0900

    feat: add Account.cpp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,10 +6,57 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:37:49 by yena              #+#    #+#             */
-/*   Updated: 2023/06/05 13:37:49 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 14:32:31 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by 나예진 on 2023/06/05.
-//
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) {
+	(void)initial_deposit;
+}
+
+Account::~Account(void) {
+
+}
+
+int	Account::getNbAccounts(void) {
+	return (0);
+}
+
+int	Account::getTotalAmount(void) {
+	return (0);
+}
+
+int	Account::getNbDeposits(void) {
+	return (0);
+}
+
+int	Account::getNbWithdrawals(void) {
+	return (0);
+}
+
+int	getNbWithdrawals(void) {
+	return (0);
+}
+
+void	Account::displayAccountsInfos(void) {
+
+}
+
+void	Account::makeDeposit(int deposit) {
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal) {
+	(void)withdrawal;
+	return (true);
+}
+
+int		Account::checkAmount(void) const {
+	return (0);
+}
+
+void	Account::displayStatus(void) const {
+
+}

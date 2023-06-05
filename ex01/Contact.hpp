a740a5fd904bff0b47ca5d413b1e9f97cb71512c
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 11:38:26 +0900

    feat: add doxygen and getter/setter

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:06:56 by yena              #+#    #+#             */
-/*   Updated: 2023/05/31 14:52:06 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 11:35:26 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 
 #include <iostream>
 
+/**
+ * @class Contact
+ * @brief Contact class
+ */
 class Contact
 {
 	private:
-		std::string first_name;
-		std::string last_name;
-		std::string nickname;
-		std::string phone_number;
-		std::string darkest_secret;
+		std::string first_name;		///< First name
+		std::string last_name;		///< Last name
+		std::string nickname;		///< Nickname
+		std::string phone_number;	///< Phone number
+		std::string darkest_secret;	///< Darkest secret
 
 	public:
 		Contact(void);
@@ -30,6 +34,16 @@ class Contact
 				std::string nickname, std::string phone_number,
 				std::string darkest_secret);
 		~Contact(void);
+		std::string GetFirstName(void);
+		std::string GetLastName(void);
+		std::string GetNickname(void);
+		std::string GetPhoneNumber(void);
+		std::string GetDarkestSecret(void);
+		void SetFirstName(std::string first_name);
+		void SetLastName(std::string last_name);
+		void SetNickname(std::string nickname);
+		void SetPhoneNumber(std::string phone_number);
+		void SetDarkestSecret(std::string darkest_secret);
 };
 
 Contact::Contact(void)
@@ -52,6 +66,58 @@ Contact::Contact(std::string first_name, std::string last_name,
 Contact::~Contact(void)
 {
 
+}
+
+// Getter
+std::string Contact::GetFirstName(void)
+{
+	return (this->first_name);
+}
+
+std::string Contact::GetLastName(void)
+{
+	return (this->last_name);
+}
+
+std::string Contact::GetNickname(void)
+{
+	return (this->nickname);
+}
+
+std::string Contact::GetPhoneNumber(void)
+{
+	return (this->phone_number);
+}
+
+std::string Contact::GetDarkestSecret(void)
+{
+	return (this->darkest_secret);
+}
+
+// Setter
+void	Contact::SetFirstName(std::string first_name)
+{
+	this->first_name = first_name;
+}
+
+void	Contact::SetLastName(std::string last_name)
+{
+	this->last_name = last_name;
+}
+
+void	Contact::SetNickname(std::string nickname)
+{
+	this->nickname = nickname;
+}
+
+void	Contact::SetPhoneNumber(std::string phone_number)
+{
+	this->phone_number = phone_number;
+}
+
+void	Contact::SetDarkestSecret(std::string darkest_secret)
+{
+	this->darkest_secret = darkest_secret;
 }
 
 #endif
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:06:45 by yena              #+#    #+#             */
-/*   Updated: 2023/05/31 14:55:38 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 11:37:17 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,56 @@
 
 #include "Contact.hpp"
 
+/**
+ * @class PhoneBook
+ * @brief PhoneBook class
+ */
 class PhoneBook
 {
 	private:
-		Contact contacts[8];
+		Contact	contacts[8];	///< Contacts
+		int		capacity;		///< Capacity of PhoneBook. If capacity is 8, PhoneBook is full.
 
 	public:
 		PhoneBook(void);
-		PhoneBook(Contact contact, int index);
 		~PhoneBook(void);
+		Contact	GetContact(int index);
+		int		GetCapacity(void);
+		void	SetContact(Contact contact, int index);
+		void	SetCapacity(int index);
 };
 
-PhoneBook::PhoneBook(Contact contact, int index)
+PhoneBook::PhoneBook()
 {
-	this->contacts[index] = contact;
+	this->capacity = 0;
+	for (int i = 0; i < 8; i++)
+		this->contacts[i] = Contact();
 }
 
 PhoneBook::~PhoneBook(void)
 {
 }
+
+// Getter
+Contact	PhoneBook::GetContact(int index)
+{
+	return (this->contacts[index]);
+}
+
+int	PhoneBook::GetCapacity(void)
+{
+	return (this->capacity);
+}
+
+// Setter
+void	PhoneBook::SetContact(Contact contact, int index)
+{
+	this->contacts[index] = contact;
+}
+
+void	PhoneBook::SetCapacity(int capacity)
+{
+	this->capacity = capacity;
+}
+
 #endif

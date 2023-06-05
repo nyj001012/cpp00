184bbc3f2bdaf99dc0c1c9ad8a19d566fd5fb43d
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 09:00:50 +0900

    chore: change if-else to switch-case

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:06:17 by yena              #+#    #+#             */
-/*   Updated: 2023/06/05 08:55:33 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 09:00:29 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,21 @@ int	main(void)
 	{
 		std::cout << "Enter command: ";
 		std::cin >> command;
-		if (command == "EXIT")
-			break ;
-		else if (command == "ADD")
-			std::cout << "ADD" << std::endl;
-		else if (command == "SEARCH")
-			std::cout << "SEARCH" << std::endl;
-		else
-			std::cout << "Invalid command: " << command << std::endl;
+		switch (command)
+		{
+			case "ADD":
+				std::cout << "ADD" << std::endl;
+				break ;
+			case "SEARCH":
+				std::cout << "SEARCH" << std::endl;
+				break ;
+			case "EXIT":
+				std::cout << "EXIT" << std::endl;
+				break ;
+			default:
+				std::cout << "Invalid command: " << command << std::endl;
+				break ;
+		}
 	}
 	return (0);
 }

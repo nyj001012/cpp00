063056a476a7fe1ddcf66ce60d8dd4dd2c2310c3
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-05 09:04:28 +0900

    chore: change way to get input and print output

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,8 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 09:03:29 by yena              #+#    #+#             */
+/*   Updated: 2023/06/05 09:03:52 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:06:17 by yena              #+#    #+#             */
-/*   Updated: 2023/06/05 08:40:38 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/05 09:03:29 by yena             ###   ########.fr       */#.fr       */
+=======
+/*   Updated: 2023/06/05 08:55:33 by yena             ###   ########.fr       */
+>>>>>>> parent of 184bbc3 (chore: change if-else to switch-case)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +35,8 @@ int	main(void)
 
 	while (true)
 	{
-		command = std::cin.get();
-		std::cout << "command: " << command << std::endl;
+		std::cout << "Enter command: ";
+		std::cin >> command;
 		if (command == "EXIT")
 			break ;
 		else if (command == "ADD")
@@ -29,7 +44,7 @@ int	main(void)
 		else if (command == "SEARCH")
 			std::cout << "SEARCH" << std::endl;
 		else
-			std::cout << "Invalid command" << std::endl;
+			std::cout << "Invalid command: " << command << std::endl;
 	}
 	return (0);
 }
